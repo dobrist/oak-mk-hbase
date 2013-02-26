da509755972dca65433d817348c4ee680a30d9c6
Damien Obrist <damien.obrist@gmail.com>
2013-02-26 09:43:02 +0100

    Added test for getHeadRevision()

---
+++ src/test/java/ch/x42/terye/mk/hbase/GetHeadRevisionTest.java
@@ -0,0 +1,25 @@
+package ch.x42.terye.mk.hbase;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GetHeadRevisionTest extends HBaseMicroKernelTest {
+
+    @Test
+    public void testInitialRevision() {
+        assertEquals("0", microKernel.getHeadRevision());
+    }
+
+    @Test
+    public void testAfterCommit() throws Exception {
+        // add one node
+        scenario.addNode("/node");
+        String r = scenario.commit();
+        // wait for the microkernel to see the commit
+        Thread.sleep(WAIT_TIMEOUT);
+        // verify
+        assertEquals(r, microKernel.getHeadRevision());
+    }
+
+}
